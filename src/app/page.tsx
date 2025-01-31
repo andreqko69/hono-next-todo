63d04035479f63b7f0040b713132823e0197f708
andreqko <andreqkodev@gmail.com>
2025-01-31 20:21:39 +0200

    fix: redirect on /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,7 @@
-'use client';
+import { redirect } from 'next/navigation';
 
-import { useEffect, useState } from 'react';
+import { Route } from '@/shared/navigation/constants';
 
 export default function Home() {
-  const [todos, setTodos] = useState<{ id: number; title: string }[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('/api/todo');
-      const { todos } = await res.json();
-      console.log('todos:', todos);
-      setTodos(todos);
-    };
-
-    fetchData();
-  }, []);
-
-  if (todos.length === 0) return <p>Loading...</p>;
-
-  return (
-    <div>
-      {todos.map((todo) => (
-        <div key={todo.id}>{todo.title}</div>
-      ))}
-    </div>
-  );
+  redirect(Route.Dashboard);
 }
